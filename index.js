64a1ff5eeed212c79e68aa1ae8e445e2704db09f
Rohit Kataria <rohit.kataria@sentry.io>
2021-02-19 10:26:05 -0800

    changed

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node. Copied Code!\n'
+  const msg = 'Hello Node. Copied Code changed!\n'
   res.end(msg);
 });
 
