32471e19ed136f9911a29053b13076bdb4382db2
Rohit Kataria <rohit.kataria@sentry.io>
2021-02-19 10:28:23 -0800

    one more change

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
-  const msg = 'Hello Node. No more code!\n'
+  const msg = 'Hello Node. Small change!\n'
   res.end(msg);
 });
 
