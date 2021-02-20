6bd02c8e260247bb51ba045b36add8658565d4ca
Rohit Kataria <rohit.kataria@sentry.io>
2021-02-20 08:37:28 -0800

    index chnages

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 const Sentry = require("@sentry/node");
 Sentry.init({
   dsn: "https://df0c4554cf6b489d839688c29b0f255d@o87286.ingest.sentry.io/5641149",
+  release: "node-hello-1.0" 
 });
 
-undefinedFunction();
+uncoolFunction();
 
 const http = require('http');
 const port = process.env.PORT || 3000;
@@ -15,7 +16,7 @@ const server = http.createServer((req, res) => {
 });
 
 try {
- console.log("hello");
+ console.log("some message");
 } catch (error) {
   console.error(error);
 }
