4602332221b5a8a514f96c7ceb71b59560092e2a
Rohit Kataria <rohit.kataria@sentry.io>
2021-02-19 16:46:41 -0800

    commiting

--- index.js
+++ index.js
@@ -3,8 +3,6 @@ Sentry.init({
   dsn: "https://df0c4554cf6b489d839688c29b0f255d@o87286.ingest.sentry.io/5641149",
 });
 
-undefinedFuntion();
-
 const http = require('http');
 const port = process.env.PORT || 3000;
 
