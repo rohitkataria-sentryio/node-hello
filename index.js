12917b8991ada7492805dbff53987345e1abe2af
Rohit Kataria <rohit.kataria@sentry.io>
2021-02-21 06:15:42 -0800

    fix release version

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const Sentry = require("@sentry/node");
 Sentry.init({
   dsn: "https://df0c4554cf6b489d839688c29b0f255d@o87286.ingest.sentry.io/5641149",
-  release: "node-hello-1.0" 
+  release: process.env.RELEASE_VERSION 
 });
 
-uncoolFunction();
+uncoolFunctionagain();
 
 const http = require('http');
 const port = process.env.PORT || 3000;
@@ -16,7 +16,7 @@ const server = http.createServer((req, res) => {
 });
 
 try {
- console.log("some message");
+ console.log(process.env.RELEASE_VERSION);
 } catch (error) {
   console.error(error);
 }
